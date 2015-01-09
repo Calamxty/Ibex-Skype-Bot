c194682cfe009bd2555c458ef0a790f908c71518
Calamity <Twitter-@Calamxty>
2015-01-08 17:54:39 -0800

    Files
    Skype bot, config file, other text files

---
+++ config.py
@@ -0,0 +1,66 @@
+""" These you can edit however you want. """
+
+name = "Ibex" #The bot's tag. (Ex: "[Ibex]: This is a message!")
+
+admin = ['echo123','skype.name'] #Add as many as you want. These are admins that ARENT the bot account
+
+timerGoal = '168' #By default the timer's goal is set to a week. (Ex: 0/168 HOURS)
+
+weatherKey = '' #Get one from http://www.wunderground.com/ for weather API (XML)
+
+""" This stuff is for the Skype2IRC part I made for the bot. You don't need to use it, but in case you want to... """
+server 	= 'irc.freenode.net'
+port 	= 6667
+channel = '#IbexIRC'
+botname = 'IbexIRC'
+""" These you can edit, but would recommend to leave alone """
+
+blacklist = [] #You can add words you can black list. The users will be kicked if they used these words.
+
+welcomeMSG = ['hi','hii','hiii','hiiii','hello','hey','heyy','heyyy','heyyyy','heyyyyy','hii','hola','helloo','helllo','hellloo','sup','supp','yo','yoo','yooo','yoooo','yooooo','watsup','whats up','holaa','oi','greetings'] #You can add more if you want.
+
+afkMSG = ['brb','be right back','!afk','afk','gtg','g2g','gotta go'] #You can add more if you want.
+
+askResponses = ['Signs point to yes.','Yes.','Reply hazy, try again.','Without a doubt.','My sources say no.','As I see it, yes.','You may rely on it.','Concentrate and ask again.','Outlook not so good.','It is decidedly so.','Better not tell you now.','Very doubtful.','Yes - definitely.','It is certain.','Cannot predict now.','Most likely.','Ask again later.','My reply is no.','Outlook good.','Don\'t count on it.','FIND OUT IN THE NEXT EPISODE OF DRAGON BALL Z']
+
+
+""" These you don't need to edit and you probably shouldn't """
+botAccount = '' #Leave blank. It automatically detects it.
+afkList = [] #Leave Alone, use !afk or type "brb" or "be right back" to be marked as AFK
+timer = '0' #This is for the call timer. Set it by doing !timer [number]
+""" These are functions. Don't fuck with this shit, yo. """
+import hashlib,os,random,re,urllib2
+#Thanks to my nigga AzuTaki for a better method of getting titles and the isUP command.
+def getBetween(source, start, stop):
+	data = re.compile(start + '(.*?)' + stop).search(source)
+	if data:
+		found = data.group(1)
+		return found.replace('\n', '')
+	else:
+		return False
+def getCleanURL(url):
+	cleanURL = url.replace('https://', '', 1)
+	cleanURL = cleanURL.replace('http://', '', 1)
+	cleanURL = cleanURL.replace('www.', '', 1)
+	cleanURL = 'http://' + cleanURL
+	return cleanURL
+def getTitle(url): 
+	try:
+		source = urllib2.urlopen(url).read()
+		title  = getBetween(source, '<title>', '</title>')
+		return title
+	except:
+		return False
+def isUP(url):
+	try:
+		source = urllib2.urlopen('http://www.downforeveryoneorjustme.com/' + url).read()
+		if source.find('It\'s just you.') != -1:
+			return 'Website Responsive.'
+		elif source.find('It\'s not just you!') != -1:
+			return 'Website seems to be down.'
+		elif source.find('Huh?') != -1:
+			return 'Invalid Website. Try again.'
+		else:
+			return 'UNKNOWN'
+	except:
+		return 'UNKNOWN ERROR'
---
+++ ibex.py
@@ -0,0 +1,162 @@
+""" 
+Creator			: @Calamxty (Email/XMPP: iAmCalamity@riseup.net)
+
+Description		: Ibex Skype Bot was basically made out of boredom. A side-project I'd work on whenever I'd Skype with people.
+				: It's a pretty simple bot. Some of the stuff I did could've been done more efficiently, however, I don't care.
+				: I simply just added stuff whenever I'd come up with an idea of a function/feature. 
+						
+Important Notes	: I made this on my windows computer, but a few simple changes and I've had it run on Fedora 20 as well.
+				: I came across the issue of Microsoft disabling their chat API and changing the group chat to work differently,
+				: HOWEVER, I am able to use Skype 6.10.0.104 and get it to work. It doesn't work in group chats outright, though.
+				: It started off only working in private messages, until I decided to try to add a friend through private message, creating
+				: a group that way, and the bot managed to work! It's a bit of effort, but to me it was worth it in the long run.
+
+Dependencies	: 
+				: - Skype4Py
+				:	- https://pypi.python.org/pypi/Skype4Py/
+				: - BeautifulSoup 4.3.2
+				:	- https://pypi.python.org/pypi/beautifulsoup4/4.3.2
+				: - Skype that works with Skype4Py 
+				:	- http://lmgtfy.com/?q=Skype+6.10.0.104 (Can't give you DL, but can point in right direction)
+"""
+import os,sys,time,Skype4Py,urllib2,config
+from bs4 import BeautifulSoup
+from random import choice
+s = Skype4Py.Skype()
+s.Attach() #Attach to Skype. Make sure to accept permissions from Skype client!
+config.botAccount = s.CurrentUserHandle
+tag = "["+config.name+"]: "
+os.system('cls')
+print config.name+" bot currently running on Skype account: "+config.botAccount+"\nIf any errors occur, they will be printed here."
+def inMSG(checklist, messagelol):
+    return set(checklist).intersection(messagelol.split())
+def Ibex(Message, Status):
+	chat = Message.Chat
+	members = chat.MemberObjects
+	msg = Message.Body
+	send = Message.Chat.SendMessage
+	sAlias = Message.FromDisplayName
+	sUsername = Message.FromHandle
+	roles = ['MASTER','CREATOR','ADMIN']
+	for name in members:
+		if inMSG(roles,name.Role):
+			config.admin.append(name.Handle)
+#Handling messages and AFK System and link titles
+	if Status == 'SENT' or (Status == 'RECEIVED'):
+		if inMSG(config.afkList, sUsername):
+			config.afkList.remove(sUsername)
+			config.afkList.remove(sAlias)
+			send(tag+"Welcome back, %s! You're no longer marked as AFK." % sAlias)
+		if inMSG(config.welcomeMSG, msg.lower()) and sUsername != config.botAccount:
+			send(tag+"Hello, %s!" % sAlias)
+		if inMSG(config.afkMSG, msg.lower()):
+			config.afkList.append(sUsername)
+			config.afkList.append(sAlias)
+			send(tag+"You are now marked as AFK, %s!\nUsers who try to contact you will be notified.\nType anything to be marked as back." % sAlias)
+		if inMSG(config.afkList, msg.lower()):
+			send(tag+"That person is AFK.")
+		if inMSG(config.blacklist, msg.lower()):
+			send(tag+"Do not say that word!")
+			time.sleep(1)
+			send("/kick %s" % sUsername)
+		if msg.startswith('https://') or msg.startswith('http://') or msg.startswith('www.'):
+			url = config.getCleanURL(msg)
+			titleCheck = config.getTitle(url)
+			titleCheck = titleCheck.replace('', '', 1)
+			send('[Title]: ' + titleCheck)
+#Commands
+		if msg.lower() == '!ping':
+			send(tag+"PONG. Connection active. (dance)")
+		if msg.lower() == '!help':
+			help = open(os.getcwd() + '\\db\\help.txt','r')
+			send(help)
+		if msg.lower() == '!rules':
+			rules = open(os.getcwd() + '\\db\\rules.txt','r')
+			send(rules)
+		if msg.lower() == '!commands':
+			commands = open(os.getcwd() + '\\db\\cmds.txt','r')
+			send(commands)
+		if msg.lower() == '!info':
+			if chat.Topic == '':
+				chatName = chat.Name
+			else:
+				chatName = chat.Topic
+			send(tag+'Current Call Timer: '+config.timer+'/'+config.timerGoal+' HOURS\nTopic: '+chatName+'\nCall Status: '+callStatus+'\nMessage Count: ' + str(len(chat.Messages))+'\nMember Count: '+str(len(chat.Members)) +'\nBot Account: '+config.botAccount)
+		if msg.lower().startswith('!timer '):
+			timer = msg.replace('!timer ','',1)
+			config.timer = timer
+			send('/topic '+timer+'/'+config.timerGoal+' HOURS | Call Record: 125 Hours')
+		if msg.lower() == '!time':
+			send('Current Call Time: '+config.timer)
+		if msg.lower().startswith('!ask '):
+			question = msg.replace('!ask ','',1)
+			response = choice(config.askResponses)
+			send(tag+'Question Asked: '+question+'\n[Answer]: '+response)
+		if msg.lower().startswith('!isup '):
+			check = msg.replace('!isup ', '', 1)
+			url = config.getCleanURL(check)
+			send(tag + config.isUP(url))
+		if msg.lower().startswith('!ping '):
+			check = msg.replace('!ping ', '', 1)
+			ping = urllib2.urlopen('http://api.hackertarget.com/nping/?q='+check).read()
+			send(tag+ ping)
+		if msg.lower().startswith('!tinyurl '):
+			url = msg.replace('!tinyurl ', '', 1)
+			url = config.getCleanURL(url)
+			tiny = urllib2.urlopen('http://tinyurl.com/api-create.php?url=' + url).read()
+			send(tag+tiny)
+		if msg.lower().startswith('!trace '):
+			ip = msg.replace('!trace ', '', 1)
+			xml = urllib2.urlopen('http://freegeoip.net/xml/'+ip).read()
+			ipinfo = BeautifulSoup(xml)
+			send(tag+'Target: '+ip+' traced by '+senderDisplay+' ('+senderHandle+'). \n\nIP Address: %s' % ipinfo.ip.text+'\nCountry: %s' % ipinfo.countryname.text+'\nState: %s' % ipinfo.regionname.text+'\nCity: %s' % ipinfo.city.text+'\nZip Code: %s' % ipinfo.zipcode.text+'\nLatitude: %s' % ipinfo.latitude.text +'\nLongitude: %s' % ipinfo.longitude.text + '\nArea Code: %s' % ipinfo.areacode.text)
+		if msg.lower().startswith('!weather '):
+			if config.weatherKey != '':
+				location = msg.replace('!weather ','',1)
+				xml2 = urllib2.urlopen('http://api.wunderground.com/api/'+config.weatherKey+'/conditions/q/'+location+'.xml').read() 
+				weather = BeautifulSoup(xml2)
+				send(tag+'Weather information for '+location+' requested by '+senderDisplay+'\n\nLocation: %s' %weather.observation_location.city.text+'\nCurrent Temp: %s' % weather.temperature_string.text + ' | '+ weather.weather.text + '\n%s' % weather.observation_time.text)
+			else:
+				send(tag+'No weather key found. Please read/edit config.py')
+		if msg == '!anime':
+			anime = open(os.getcwd() + '\\db\\anime.txt','r').readlines()
+			send('[Random Anime]: ' + choice(anime))
+		if msg == '!movie':
+			movie = open(os.getcwd() + '\\db\\movies.txt','r').readlines()
+			send('[Random Movie]: ' + choice(movie))
+		if msg.startswith('!topic '):
+			topic = msg.replace('!topic ','',1)
+			if topic.startswith('!'):
+				send('Please do not start the topic with \'!\'. Thank you.')
+			else:
+				send('/topic '+topic+' | Call Record: 125 Hours')
+	if Status == 'SENT' or inMSG(config.admin, sUsername):
+		if msg.lower() == '!lock topic':
+			send('/set options +TOPIC_AND_PIC_LOCKED_FOR_USERS')
+		if msg.lower() == '!nuke':
+			send('/topic Chat Nuked by %s' % sAlias)
+			time.sleep(1)
+			send('Chat being nuked in 5..4..3..2..1..')
+			time.sleep(5)
+			for name in members:
+				kick = name.Handle
+				if inMSG(config.admin,kick):
+					pass
+				else:
+					send('/kick '+kick)
+			send('Nuke successful. Admins left in the chat. Please type /leave')
+		if msg.startswith('!kick '):
+			kick = msg.replace('!kick ','',1)
+			send('You are being kicked, '+kick)
+			time.sleep(1)
+			send('/kick '+kick)
+		if msg.startswith('!ban '):
+			ban = msg.replace('!ban ','',1)
+			send('You are being banned, '+ban)
+			time.sleep(1)
+			send('/kickban '+ban)
+			
+
+s.OnMessageStatus = Ibex
+while True:
+    raw_input('')
